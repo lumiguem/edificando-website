c20f576cfc620737feca5ff1f68fca01a2f6b4c9
lumiguem <miguel.morenor@icloud.com>
2025-11-27 08:09:56 -0500

    .env added - api_base_url nueva

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import type {Category, ServiceType, Product, Service} from '../types';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 // -----------------------------
 // HELPERS
